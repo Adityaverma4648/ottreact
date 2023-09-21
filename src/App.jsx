45bf4b5de2a3b16b8922d29dfe131cd58ab59caa
Adityaverma4648 <adityaverma4648@gmail.com>
2023-09-21 12:39:06 +0530

    router tweeked

--- src/App.jsx
+++ src/App.jsx
@@ -25,24 +25,24 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Welcome />} initial></Route>
-          <Route path="/home" element={<Home />} exact></Route>
-          <Route path="/login" element={<Login />} exact></Route>
-          <Route path="/register" element={<Register />} exact></Route>
+          <Route path="home" element={<Home />} exact></Route>
+          <Route path="login" element={<Login />} exact></Route>
+          <Route path="register" element={<Register />} exact></Route>
 
           {/*  single Shorts */}
-          <Route path="/shorts/:id" element={<Shorts />}></Route>
+          <Route path="shorts/:id" element={<Shorts />}></Route>
 
           {/*  single Genre */}
-          <Route path="/genre/:id" element={<Genre />}></Route>
+          <Route path="genre/:id" element={<Genre />}></Route>
 
           {/* singlePeople */}
           <Route path="/people/:id" element={<PeopleUnique />}></Route> 
 
 
-          <Route path="/search" element={<Search />}></Route>
-          <Route path="/watchlater" element={<WatchLater />}></Route>
-          <Route path="/movies" element={<Movies />}></Route>
-          <Route path="/tvshows" element={<TVShows />}></Route>
+          <Route path="search" element={<Search />}></Route>
+          <Route path="watchlater" element={<WatchLater />}></Route>
+          <Route path="movies" element={<Movies />}></Route>
+          <Route path="tvshows" element={<TVShows />}></Route>
           <Route path="saved" element={<Saved />}></Route>
 
         </Routes>
